d47e87b5e5830dc5e5f77f1c61033016034b1bae
demian44 <demi-44@hotmail.com>
2018-07-30 22:07:48 -0300

    Inicio el proyecto_

--- wheather-app/src/components/WeatherTemperature.js
+++ wheather-app/src/components/WeatherTemperature.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
-const WeatherTemperature = () => <div><span>45°C</span></div>;
+const WeatherTemperature = () => 
+<div><span>45°C</span></div>;
 
 export default WeatherTemperature;
